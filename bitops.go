fe52213ac313c8374298c9c34c84a23c8be51c91
Chih-Min Chao <cmchao@gmail.com>
2015-02-15 00:10:36 +0800

    api : implemnt [Deposit|SetField] x [32|64]
        close #1
    
        Deposit32/64 : start and length form
        SetField32/64 : high and low form

--- bitops.go
+++ bitops.go
@@ -55,3 +55,55 @@ func GetField64(value uint64, high uint, low uint) (uint64, error) {
 
     return extract64(value, low, high - low + 1)
 }
+
+// real implementation for Depoit32 and SetField32
+func deposit32(value uint32, start uint, length uint, field uint32) (uint32, error) {
+    mask := (^uint32(0) >> (32 - length)) << start;
+    return (value & ^mask) | ((field << start) & mask), nil;
+}
+
+// real implementation for Depoit64 and SetField64
+func deposit64(value uint64, start uint, length uint, field uint64) (uint64, error) {
+    mask := (^uint64(0) >> (64 - length)) << start;
+    return (value & ^mask) | ((field << start) & mask), nil;
+}
+
+// Deposit32 specified field to uint32  variable by staring position and length
+// LSB/MSB are 0/31 and return original value if error occurs
+func Deposit32(value uint32, start uint, length uint, field uint32) (uint32, error) {
+    if start > 31 || length > 32 - start {
+        return value, fmt.Errorf("invalid start(%v) or length(%v)", start, length);
+    }
+
+    return deposit32(value, start, length, field)
+}
+
+// Deposit64 specified field to uint64  variable by staring position and length
+// LSB/MSB are 0/63 and return original value if error occurs
+func Deposit64(value uint64, start uint, length uint, field uint64) (uint64, error) {
+    if start > 63 || length > 64 - start {
+        return value, fmt.Errorf("invalid start(%v) or length(%v)", start, length);
+    }
+
+    return deposit64(value, start, length, field)
+}
+
+// SetField32 specified field to uint32 variable by staring position and length
+// LSB/MSB are 0/31 and return original value if error occurs
+func SetField32(value uint32, high uint, low uint, field uint32) (uint32, error) {
+    if high > 31  || low > 31 || high < low {
+        return value, fmt.Errorf("invalid high(%v) or low(%v)", high, low);
+    }
+
+    return deposit32(value, low, high - low + 1, field)
+}
+
+// SetField64 specified field to uint64 variable by staring position and length
+// LSB/MSB are 0/63 and return original value if error occurs
+func SetField64(value uint64, high uint, low uint, field uint64) (uint64, error) {
+    if high > 63  || low > 63 || high < low {
+        return value, fmt.Errorf("invalid high(%v) or low(%v)", high, low);
+    }
+
+    return deposit64(value, low, high - low + 1, field)
+}
--- bitops_test.go
+++ bitops_test.go
@@ -230,3 +230,282 @@ func TestGetField64(t *testing.T) {
     }
 }
 
+func TestDeposit32(t *testing.T) {
+    var value uint32 = 0xF0F0F0F0
+    var length uint
+    var start uint
+    var field uint32
+
+    //check error
+    start = 32
+    length = 0
+    field, err := Deposit32(value, start, length, 0x1234)
+    if err == nil {
+        t.Fail()
+        t.Log("invalid start")
+    }
+
+    if value != field {
+        t.Fail()
+        t.Logf("invalid return %x", field)
+    }
+
+    start = 0
+    length = 33
+    field, err = Deposit32(value, start, length, 0x1234)
+    if err == nil {
+        t.Fail()
+        t.Log("invalid length")
+    }
+
+    if value != field {
+        t.Fail()
+        t.Log("invalid return")
+    }
+
+    start = 31
+    length = 2
+    field, err = Deposit32(value, start, length, 0x1234)
+    if err == nil {
+        t.Fail()
+        t.Log("invalid length from valid start")
+    }
+    if value != field {
+        t.Fail()
+        t.Log("invalid return")
+    }
+
+    //check pass case
+    start = 0
+    length = 1
+    field, err = Deposit32(value, start, length, 0x7)
+    if err != nil || field != 0xF0F0F0F1 {
+        t.Fail()
+        t.Log("LSB")
+    }
+
+    start = 31
+    length = 1
+    field, err = Deposit32(value, start, length, 0x0)
+    if err != nil || field != 0x70F0F0F0 {
+        t.Fail()
+        t.Log("MSB")
+    }
+
+    start = 4
+    length = 16
+    field, err = Deposit32(value, start, length, 0x1234)
+    if err != nil || field != 0xF0F12340 {
+        t.Fail()
+        t.Log("get valid field")
+    }
+}
+
+func TestDeposit64(t *testing.T) {
+    var value uint64 = 0xF0F0F0F0F0F0F0F0
+    var length uint
+    var start uint
+    var field uint64
+
+    //check error
+    start = 64
+    length = 0
+    field, err := Deposit64(value, start, length, 0x1234)
+    if err == nil {
+        t.Fail()
+        t.Log("invalid start")
+    }
+
+    if value != field {
+        t.Fail()
+        t.Logf("invalid return %x", field)
+    }
+
+    start = 0
+    length = 65
+    field, err = Deposit64(value, start, length, 0x1234)
+    if err == nil {
+        t.Fail()
+        t.Log("invalid length")
+    }
+
+    if value != field {
+        t.Fail()
+        t.Log("invalid return")
+    }
+
+    start = 63
+    length = 2
+    field, err = Deposit64(value, start, length, 0x1234)
+    if err == nil {
+        t.Fail()
+        t.Log("invalid length from valid start")
+    }
+    if value != field {
+        t.Fail()
+        t.Log("invalid return")
+    }
+
+    //check pass case
+    start = 0
+    length = 1
+    field, err = Deposit64(value, start, length, 0x7)
+    if err != nil || field != 0xF0F0F0F0F0F0F0F1 {
+        t.Fail()
+        t.Log("LSB")
+    }
+
+    start = 63
+    length = 1
+    field, err = Deposit64(value, start, length, 0x0)
+    if err != nil || field != 0x70F0F0F0F0F0F0F0 {
+        t.Fail()
+        t.Log("MSB")
+    }
+
+    start = 4
+    length = 16
+    field, err = Deposit64(value, start, length, 0x1234)
+    if err != nil || field != 0xF0F0F0F0F0F12340 {
+        t.Fail()
+        t.Log("get valid field")
+    }
+}
+
+func TestSetField32(t *testing.T) {
+    var value uint32 = 0xF0F0F0F0
+    var high uint
+    var low uint
+    var field uint32
+
+    //check error
+    high = 32
+    low = 0
+    field, err := SetField32(value, high, low, 0)
+    if err == nil {
+        t.Fail()
+        t.Log("invalid high")
+    }
+    if value != field {
+        t.Fail()
+        t.Logf("invalid return %x", field)
+    }
+
+    high = 0
+    low = 32
+    field, err = SetField32(value, high, low, 0)
+    if err == nil {
+        t.Fail()
+        t.Log("invalid low")
+    }
+    if value != field {
+        t.Fail()
+        t.Log("invalid return")
+    }
+
+    high = 10
+    low = 20
+    field, err = SetField32(value, high, low, 0)
+    if err == nil {
+        t.Fail()
+        t.Log("high < low")
+    }
+    if value != field {
+        t.Fail()
+        t.Log("invalid return")
+    }
+
+    //check pass case
+    high = 0
+    low = 0
+    field, err = SetField32(value, high, low, 0x7)
+    if err != nil || field != 0xF0F0F0F1 {
+        t.Fail()
+        t.Log("LSB")
+    }
+
+    high = 31
+    low = 31
+    field, err = SetField32(value, high, low, 0x0)
+    if err != nil || field != 0x70F0F0F0 {
+        t.Fail()
+        t.Log("MSB")
+    }
+
+    high = 19
+    low = 4
+    field, err = SetField32(value, high, low, 0x1234)
+    if err != nil || field != 0xF0F12340 {
+        t.Fail()
+        t.Log("get valid field")
+    }
+}
+
+func TestSetField64(t *testing.T) {
+    var value uint64 = 0xF0F0F0F0F0F0F0F0
+    var high uint
+    var low uint
+    var field uint64
+
+    //check error
+    high = 64
+    low = 0
+    field, err := SetField64(value, high, low, 0)
+    if err == nil {
+        t.Fail()
+        t.Log("invalid high")
+    }
+    if value != field {
+        t.Fail()
+        t.Logf("invalid return %x", field)
+    }
+
+    high = 0
+    low = 64
+    field, err = SetField64(value, high, low, 0)
+    if err == nil {
+        t.Fail()
+        t.Log("invalid low")
+    }
+    if value != field {
+        t.Fail()
+        t.Log("invalid return")
+    }
+
+    high = 10
+    low = 20
+    field, err = SetField64(value, high, low, 0)
+    if err == nil {
+        t.Fail()
+        t.Log("high < low")
+    }
+    if value != field {
+        t.Fail()
+        t.Log("invalid return")
+    }
+
+    //check pass case
+    high = 0
+    low = 0
+    field, err = SetField64(value, high, low, 0x7)
+    if err != nil || field != 0xF0F0F0F0F0F0F0F1 {
+        t.Fail()
+        t.Log("LSB")
+    }
+
+    high = 63
+    low = 63
+    field, err = SetField64(value, high, low, 0x0)
+    if err != nil || field != 0x70F0F0F0F0F0F0F0 {
+        t.Fail()
+        t.Log("MSB")
+    }
+
+    high = 19
+    low = 4
+    field, err = SetField64(value, high, low, 0x1234)
+    if err != nil || field != 0xF0F0F0F0F0F12340 {
+        t.Fail()
+        t.Log("get valid field")
+    }
+}
